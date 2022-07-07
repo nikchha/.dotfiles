4dfe08ef9e717c455f1ad0bb89e81335c6b6f950
Niklas Hansen <niklas.hansen@enersis.de>
2022-07-07 15:11:22 +0200

    tabular nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,7 @@ end
 require('packer').startup(function(use)
   -- My plugins here
   use 'github/copilot.vim'
+  use 'godlygeek/tabular'
   use 'preservim/vim-markdown'
 
   -- Automatically set up your configuration after cloning packer.nvim
